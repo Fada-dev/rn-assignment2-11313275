d4c1811c323f91f7dc25942f90b594778b5bb086
Fada-dev <aafful_broni@st.ug.edu>
2024-05-27 14:01:29 -0700

    Edited the Text component to display my name

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Anthony</Text>
+      <Text style={styles.text}>My name is Anthony.</Text>
       <StatusBar style="auto" />
     </View>
   );
